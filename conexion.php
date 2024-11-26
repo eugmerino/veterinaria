250e603efd6b2b52dfe52f486069935cae45daec
Eugenio Merino <merinoeug118@gmail.com>
2024-11-26 14:45:30 -0600

    add conexion.php

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "veterinaria";
+    // Crear conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Verificar conexión 
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+?>
